243cf77b8a292c0dd95797e9c9237d6be304e9d8
sarahmarie1976 <sholle7@gmail.com>
2020-07-10 19:32:42 -0500

    Updated for stretch goal added Search functionality :coffee:

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,17 +1,43 @@
 import React from 'react';
 import Todo from './Todo';
-import { Form, FormGroup, Button } from 'reactstrap';
+import { Form, FormGroup, Button, Input, Card } from 'reactstrap';
 
 
 const TodoList = (props) => {
+
+    /* BEG: SearchFunctionality  */
+    const [searchTerm, setSearchTerm] = React.useState(
+        localStorage.getItem('search') || ''
+    );
+    
+    React.useEffect(() => {
+        localStorage.setItem('search', searchTerm);
+    }, [searchTerm] );
+        
+    const handleSearch = event => {
+        setSearchTerm(event.target.value);
+    };
+
+    const searchedTasks = props.tasks.filter(searchedTask => 
+        searchedTask.task.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+    /* END: SearchFunctionality  */
+
     return (
-    <Form  >
-        <FormGroup style={{margin: "10%", width: "50%", marginLeft: "25%"}}>
+      <Form  >
+        
+
+        <FormGroup style={{margin: "10%", width: "50%", marginLeft: "25%", fontSize: "20px", fontWeight: 'bold'}}>
+            <Search search={searchTerm} onSearch={handleSearch} />
+
       <div className="todo-list" >
-        {props.tasks.map((todo) => (
+        
+        {searchedTasks.map((todo) => ( 
           <Todo key={todo.id} todo={todo} toggleTodo={props.toggleTodo} />
         ))}
-        <Button  className="clear-btn" onClick={props.clearCompleted} style={{background: '#ADFF2F', color: 'black', fontWeight: 'bold', width: "40%", marginTop: "5%", fontSize:'30px'}} >
+        
+
+        <Button  className="clear-btn" onClick={props.clearCompleted} style={{background: '#ADFF2F', color: 'black', fontWeight: 'bold', width: "55%", marginTop: "5%", fontSize:'30px'}} >
           Clear Completed Tasks
         </Button>
       </div>
@@ -19,5 +45,26 @@ const TodoList = (props) => {
     </Form>
     );
   };
+
+
+  /* BEG: SearchFunctionality  */
+  const Search = ({ search, onSearch }) => {
+  
+    return (
+      <>
+        <Card style={{ background: '#191970'}}>
+        <Input 
+            id= 'search'
+            type='text'
+            placeholder='Search Here'
+            value={search}
+            onChange={onSearch}
+            />
+            </Card>
+            <br />
+      </>
+    )
+  };
+  /* END: SearchFunctionality  */
 
 export default TodoList;
